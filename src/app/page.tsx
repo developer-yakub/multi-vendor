4f4e0aa5cd5111e96a95892cbfdfc98eac97a205
developer-yakub <developeryakub256@gmail.com>
2025-02-03 17:52:14 +0530

    basic setup,shadcn ui , themeprovider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import ThemeToggle from "@/components/shared/themeToggle";
 
 export default function Home() {
   return (
-    <div className="text-3xl text-red-600">
-      my- vendor -app
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle/>
+      </div>
+      <h1 className="text-3xl text-red-600 font-barlow">my - vendor - app</h1>
+      <Button variant={"destructive"}>Click me</Button>
     </div>
   );
 }
